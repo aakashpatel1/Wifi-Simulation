4f48a19fdd8cb9ea454d2e1d0ff52829c6166c0b
Aakash Patel <patela@smu.edu>
2015-04-26 16:24:02 -0500

    Bug Fixes

--- Simulator/src/simulator/Node.java
+++ Simulator/src/simulator/Node.java
@@ -4,16 +4,20 @@ package simulator;
 import java.util.*;
 
 public class Node {
+    //INITIALIZATION LIST 
     String nodeId, status;
     Random rand = new Random();
-    int numOfFiles, waitTime, timer, fileSize;
+    int numOfFiles, waitTime, timer, fileSize; 
     Queue<File> files = new LinkedList<File>();
     File currentFile;
     
+    //Default Constructor sets nodeID to undefined, and num of packets and packet sizes to 0
     public Node() {
         this("undefined", 0, 0);
     }
     
+    //Contstructor which populates files for the node with a set size passed 
+    //in by whoever is calling constructor
     public Node(String nodeId, int numOfFiles, int fileSize) {
         status = "waiting";
         waitTime = timer = rand.nextInt(10) + 1;
@@ -29,6 +33,7 @@ public class Node {
         currentFile = files.remove();
     }
     
+    // Sends packet to a gateway
     public void sendPacket(Gateway g) {
         if (!currentFile.packets.isEmpty()) {
             g.listen(currentFile.packets.remove());        
@@ -42,11 +47,14 @@ public class Node {
         }
     }
     
+    // changes the status of the node to sending
     public void changeStatus() {
         status = "sending";
     }
     
-    public void listen(Node[] nodes) {
+    //listens to other nodes on network to see whether they are sending or not
+    // if they are not sending, it changes its status to sending
+    public void listen(LinkedList<Node> nodes) {
         for (Node n: nodes) {
             if (n.status == "sending" && n != this) {
                 this.doubleTimer();
@@ -59,33 +67,70 @@ public class Node {
         }
     }
     
-    public void modifiedListen(Node[] nodes) {
+    // checks other nodes on network to see whether the sender's packet has a 
+    //lower sequence number than this.packet.sequencenumber
+    public void modifiedListen(LinkedList<Node> nodes) {
+//        System.out.println("MODIFIED LISTEN CALLED ON " + this.nodeId + " = THIS");
         for (Node n: nodes) {
+//            System.out.println("N= " + n.nodeId);
             if (n.status == "sending" && n != this) {
-                
-                if (n.currentFile.packets.peek().sequenceNumber < this.currentFile.packets.peek().sequenceNumber) {
-                    this.setTimer();
-                    this.status = "waiting";
-                    break;
+                //if the sequence number of this.packet if less than the packet which 
+                //is currently in sending mode
+                if(n.currentFile.packets.peek() != null && this.currentFile.packets.peek() != null) {
+                    if (n.currentFile.packets.peek().sequenceNumber > this.currentFile.packets.peek().sequenceNumber) {
+                        //this.packet sequence number begins sending and the comparison node
+                        //must go to waiting mode.
+//                        System.out.println("Nodeid= " + n.nodeId + " this.currentFile.packets.peek().sequenceNumber" + n.currentFile.packets.peek().sequenceNumber);
+//                        System.out.println("Nodeid= " + this.nodeId + " this.currentFile.packets.peek().sequenceNumber= " + this.currentFile.packets.peek().sequenceNumber);
+                        this.status = "sending";
+                        n.status = "waiting";
+//                        System.out.println("This Nodeid" + this.nodeId + "status is now" + this.status);
+//                        System.out.println("N Nodeid" + n.nodeId + "status is now " + n.status);
+                        n.setTimer();
+                    }
+                    else {
+                        //the currently sending node remains sending and this resets timer and continues to wait
+                        this.setTimer();
+                        this.status = "waiting";
+//                        System.out.println("Nodeid= " + n.nodeId + " this.currentFile.packets.peek().sequenceNumber" + n.currentFile.packets.peek().sequenceNumber);
+//                        System.out.println("Nodeid= " + this.nodeId + " this.currentFile.packets.peek().sequenceNumber= " + this.currentFile.packets.peek().sequenceNumber);
+//                        System.out.println("Node Id :" + n.nodeId + " remains " + n.status);
+//                        System.out.println("Node Id :" + this.nodeId + " remains " + this.status);
+//                        System.out.println("Break Listen Function");
+                        break;
+                    }
                 }
             }
             else {
+//                System.out.println("Node Id :" + n.nodeId + " remains " + n.status);
+//                System.out.println("Node Id :" + this.nodeId + " remains " + this.status);
                 this.status = "sending";
-                n.status = "waiting";
+                if (this != n){
+                    n.status = "waiting";
+                }
             }
         }
+        for (Node n: nodes) {
+            if (n.status ==  "sending") {
+//                System.out.println("Node Id :" + n.nodeId + " is " + n.status + ". They should be sending a packing anytime now.");
+            }
+        }
+//        System.out.println("(THIS)Node Id :" + this.nodeId + " is " + this.status);
     }
     
+    //reset timer to the waitTime
     public void setTimer() {
         timer = waitTime;
     }
     
+    //timer countdown
     public void decrementTime() {
         if (timer > 0) {
             --timer;
         }
     }
     
+    //changes status to waiting
     public void changeStatusToWaiting() {
         status = "waiting";
         if (!this.files.isEmpty()) {
@@ -94,6 +139,7 @@ public class Node {
         this.setTimer();
     }
     
+    //exponential backoff
     public void doubleTimer() {
         timer *= 2;
     }
--- Simulator/src/simulator/Simulator.java
+++ Simulator/src/simulator/Simulator.java
@@ -1,14 +1,26 @@
 
 package simulator;
 
+import java.util.*;
+
+/**
+ * Simulator is used to simulate the flow of packets coming from different nodes. 
+ **/
 public class Simulator {
     
-    public static Node[] prepareSimulationEnvironment() {
+    /**
+     * creates and return a list of nodes. Eventually, the user should be able 
+     * to input the number of nodes and the file size distribution. 
+     **/    
+    public static LinkedList<Node> prepareSimulationEnvironment() {
         int numOfNodes = 10;
-        Node[] nodes =  new Node[numOfNodes];
+        LinkedList<Node> nodes = new LinkedList<Node>();
         for (int x = 0; x < numOfNodes; ++x) {
-            nodes[x] = new Node(String.valueOf(x), 10, 10);
+            nodes.add(new Node(String.valueOf(x), 11, 10));
         }      
+        for (Node n: nodes) {
+            System.out.println("Node:" + n.nodeId + "NumOfFilesLeft " + n.files.size());
+        }
 //        for (Node n: nodes) {
 //            for (File f: n.files) {
 //                f.printFile();
@@ -17,7 +29,18 @@ public class Simulator {
         return nodes;
     }
     
-    public static void simulateOriginalAlgorithm(Node[] nodes, Gateway g, int time) {
+    /**
+     * SIMULATIONS
+     * - first for loop decrements the timer of all nodes or listens to other nodes
+     *   if the timer is up.
+     * - second for loop determines which packet is going to send if any. 
+     **/
+    
+    /**
+     * Original Algorithm uses origin listen function - if someone is already using the channel, perform
+     * an exponential backoff by doubling the timer. 
+     **/
+    public static LinkedList<Node> simulateOriginalAlgorithm(LinkedList<Node> nodes, Gateway g, int time) {
         for (Node n: nodes) {
             if (n.timer != 0) {
                 n.decrementTime();
@@ -39,9 +62,25 @@ public class Simulator {
                 }
             }
         }
+        
+        // Removes node after it has nothing to send.
+        Queue<Node> finishedNodes = new LinkedList<Node>();
+        for (Node n: nodes) {
+            if (n.files.isEmpty()) {
+                finishedNodes.add(n);
+            }
+        }
+        for (Node n: finishedNodes) {
+            nodes.remove(n);
+        }
+        return nodes;
     }
     
-        public static void simulateModifiedAlgorithm(Node[] nodes, Gateway g, int time) {
+    /**
+     * Modified Algorithm
+     * uses modified listen - nodes find which packet has the lowest sequence number
+     **/
+    public static LinkedList<Node> simulateModifiedAlgorithm(LinkedList<Node> nodes, Gateway g, int time) {
         for (Node n: nodes) {
             if (n.timer != 0) {
                 n.decrementTime();
@@ -63,27 +102,41 @@ public class Simulator {
                 }
             }
         }
+        
+        // Removes node after it has nothing to send.
+        Queue<Node> finishedNodes = new LinkedList<Node>();
+        for (Node n: nodes) {
+            if (n.files.isEmpty()) {
+                finishedNodes.add(n);
+            }
+        }
+        for (Node n: finishedNodes) {
+            nodes.remove(n);
+        }
+        return nodes;
     }
 
     public static void main(String[] args) {
-        String simulation = "original";
+        String simulation = "modified"; // determine which algorithm will be used.
         Gateway g = new Gateway();
         int time = 0;
-        Node[] nodes = prepareSimulationEnvironment();
+        LinkedList<Node> nodes = prepareSimulationEnvironment();
         
         for (Node n: nodes) {
             System.out.println(n.nodeId + ": " + n.timer);
         }
         
+        // while loop simulates one slot of time. It doesn't 
+        //stop until 1000 packets have been sent to the
+        //gateway. 
         while (g.packets.size() < 1000) {
-            
-            //System.out.println("time: " +  time);
+//            System.out.println("time: " +  time);
             if (simulation == "original") {
-                simulateOriginalAlgorithm(nodes, g, time);
+                nodes = simulateOriginalAlgorithm(nodes, g, time);
             }
             
             if (simulation == "modified") {
-                simulateModifiedAlgorithm(nodes, g, time);
+                nodes = simulateModifiedAlgorithm(nodes, g, time);
             }
             
             
@@ -91,7 +144,7 @@ public class Simulator {
             ++time;
         }
         
-        System.out.println("Gateway Packets");
+        //System.out.println("Gateway Packets");
         //g.printPackets();
         
     }
